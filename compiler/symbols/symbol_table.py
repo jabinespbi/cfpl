fc0ad6631d329856b1c3124190aed29c37ea623e
jabinespbi <jabinespbi@gmail.com>
2019-09-16 22:07:19 +0800

    Create symbol table

--- compiler/symbols/symbol_table.py
+++ compiler/symbols/symbol_table.py
@@ -1,2 +1,16 @@
 class SymbolTable:
     """"""
+    __instance = None
+    unknown_tokens = {}   # tokens such as variables, literals, etc. are not known to grammar
+
+    @staticmethod
+    def getInstance():
+        if SymbolTable.__instance is None:
+            SymbolTable()
+        return SymbolTable.__instance
+
+    def __init__(self):
+        if SymbolTable.__instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            SymbolTable.__instance = self
